cd65f76771077974c3b34643db63e0f88e962e59
pratik <pratiktiwari3792@gmail.com>
2023-02-21 16:57:47 +0530

    Added post route for tours api

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 const app = express();
 
+app.use(express.json());
+
 // app.get("/", (req, res) => {
 //   res
 //     .status(200)
@@ -26,7 +28,27 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
-const port = 3000;
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body);
+  const newId = toursObj.at(-1).id + 1;
+  const newTour = { id: newId, ...req.body };
+  toursObj.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursObj),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  // res.send("Done");
+});
+
+const port = 8000;
 app.listen(port, () => {
   console.log(`App running on port: ${port}`);
 });
