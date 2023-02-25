5dce519e10313d63864c88019bd1bb81de17e685
pratik <pratiktiwari3792@gmail.com>
2023-02-25 14:39:05 +0530

    Added mongoose for databse modelling

--- server.js
+++ server.js
@@ -1,8 +1,58 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({
   path: "./config.env",
 });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("DB connection successful");
+    // console.log(con.connections);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = require("./app");
 
